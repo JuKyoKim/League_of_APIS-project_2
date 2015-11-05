cf8b1c3709c5890809a4c06674979e034d99f153
jukyokim <jukyokim1992@gmail.com>
2015-11-05 12:02:19 -0500

    generated controllers user accounts creation

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,40 @@
+class UserController < ApplicationController
+ 	
+ 	def index
+ 	
+ 	end
+
+ 	def show
+ 	
+ 	end
+
+ 	
+ 	def new
+ 	
+ 	end
+
+ 	def create
+ 		
+ 	end
+
+ 	
+ 	def edit
+ 	
+ 	end
+
+ 	def update
+ 		
+ 	end
+
+ 	def destroy
+ 		
+ 	end
+
+ 	private
+
+ 	def user_params
+ 		params.require(:user).permit(:name, :email, :password_digest)
+ 	end
+ 	
+
+end
